7f358520b524f28f0de34c1e5a743804b439c38f
Ashutosh Mishra <theawesomeashutosh@gmail.com>
2021-04-22 21:27:05 +0530

    -

---
+++ runit-first time-n.bat
@@ -0,0 +1,1 @@
+echo "# totest" >> README.md && git init && git add README.md & git commit -m "first commit" & git branch -M main & git remote add origin https://github.com/mrakm/all-react-project.git & git push -u origin main & pause
